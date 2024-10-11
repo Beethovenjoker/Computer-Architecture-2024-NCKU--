c800bf9e22c6d6e4977212051d2dfe2d686415fa
Beethovenjoker <c11544211690@gmail.com>
2024-10-11 22:51:28 +0800

    Delete fabsf directory

--- fabsf/fabsf_in_C.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-static inline float fabsf(float x)
-{
-    uint32_t i = *(uint32_t *)&x; // Read the bits of the float into an integer
-    i &= 0x7FFFFFFF;              // Clear the sign bit to get the absolute value
-    x = *(float *)&i;             // Write the modified bits back into the float
-    return x;
-}
